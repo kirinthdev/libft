566902d53c491d37227994d1b0363d0a8361e17f
SergioRuntime <sergioruntime@hotmail.com>
2024-02-16 10:36:39 +0100

    split function

--- ft_split.c
+++ ft_split.c
@@ -6,80 +6,92 @@
 /*   By: sergmart <sergiomga136@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 11:01:14 by sergmart          #+#    #+#             */
-/*   Updated: 2024/02/13 17:18:22 by sergmart         ###   ########.fr       */
+/*   Updated: 2024/02/13 17:29:20 by sergmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	count_c(char const *s, char c)
+static void	free_all(char **arr, int j)
+{
+	while (j >= 0)
+	{
+		free(arr[j]);
+		j--;
+	}
+}
+
+static size_t	strchrlen(size_t start, char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[start + i] != c && s[start + i] != '\0')
+		i++;
+	return (i);
+}
+
+static size_t	count_str(char const *s, char c)
 {
 	size_t	count;
 
 	count = 0;
 	while (*s)
 	{
-		while (*s && *s == c)
-			s++;
 		if (*s && *s != c)
 		{
 			count++;
 			while (*s && *s != c)
 				s++;
 		}
+		while (*s && *s == c)
+			s++;
 	}
 	return (count + 1);
 }
 
-size_t	len_of_c(size_t start, char const *s, char c)
-{
-	size_t	len_c;
-
-	len_c = 0;
-	while (s[start + len_c] != c && s[start + len_c] != '\0')
-		len_c++;
-	return (len_c);
-}
-
-int	split_strings(char const *s, char c, char **str, size_t count)
+static int	input_strings(char **arr, char const *s, size_t count, char c)
 {
 	size_t	i;
-	size_t	start;
-	size_t	len_c;
+	int		j;
+	size_t	len;
 
 	i = 0;
-	start = 0;
-	while (i < count - 1)
+	j = 0;
+	while (j < (int)count - 1)
 	{
-		while (s[start] == c)
-			start++;
-		len_c = len_of_c(start, s, c);
-		str[i] = (char *)malloc((len_c + 1) * sizeof(char));
-		if (!str[i])
+		while (s[i] == c)
+			i++;
+		len = strchrlen(i, s, c);
+		arr[j] = (char *)malloc((len + 1) * sizeof(char));
+		if (!arr[j])
+		{
+			free_all(arr, j);
 			return (0);
-		ft_strlcpy(str[i], s + start, len_c + 1);
-		start = start + len_c;
-		i++;
+		}
+		ft_strlcpy(arr[j], s + i, len + 1);
+		i = i + len;
+		j++;
 	}
-	str[count - 1] = NULL;
+	arr[count - 1] = NULL;
 	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char **str;
+	char **arr;
 	size_t count;
 
-	count = count_c(s, c);
-	str = (char **)malloc(count * sizeof(char *));
-	if (!str)
+	if (!s)
 		return (NULL);
-	if (!split_strings(s, c, str, count))
+	count = count_str(s, c);
+	arr = (char **)malloc(count * sizeof(char *));
+	if (!arr)
+		return (0);
+	if (!input_strings(arr, s, count, c))
 	{
-		while (count--)
-			free(str[count]);
-		free(str);
+		free(arr);
 		return (NULL);
 	}
-	return (str);
+	return (arr);
 }
